48bf1d280bdef2692f31f486d721b25aec5216c0
Willian Ferreira <williangf@hotmail.com>
2022-10-05 20:36:07 -0300

    feat: added Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export default styled.button`
+  height: 40px;
+  padding: 0px 12px;
+  border-radius: 8px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px,
+    rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
+  transition: box-shadow 0.2s;
+
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 3px 0px,
+      rgba(0, 0, 0, 0.06) 0px 2px 2px 0px;
+  }
+
+  &:active {
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 3px 0px,
+      rgba(0, 0, 0, 0.06) 0px 0px 2px 0px;
+  }
+`;
