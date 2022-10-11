030c001a7b41c817f6ed7794126c13c52ddfc525
Willian Ferreira <williangf@hotmail.com>
2022-10-11 16:53:41 -0300

    feat: added VideoDetailsPage

---
+++ src/pages/video/[id].jsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import VideosService from "@services/VideosService";
+import PageContainer from "@components/PageContainer";
+import Button from "@components/Button";
+import Container from "@styles/pages/videoDetails";
+import { TbArrowBackUp, TbMoodSad, TbThumbUp } from "react-icons/tb";
+import DisplayMessage from "@components/DisplayMessage";
+import Loading from "@components/Loading";
+
+export default function VideoDetails() {
+  const router = useRouter();
+  const { id: videoId } = router.query;
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [videoDetails, setVideoDetails] = useState(null);
+
+  useEffect(() => {
+    async function getVideoDetails() {
+      setIsLoading(true);
+
+      try {
+        const videoDetails = await VideosService.getVideo(videoId);
+
+        setVideoDetails(videoDetails);
+      } catch (error) {
+        setError("Houve um erro ao carrega o vídeo");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    if (videoId) getVideoDetails();
+  }, [videoId]);
+
+  return (
+    <PageContainer title={videoDetails?.items[0]?.snippet.title || 'Erro'}>
+      <Container>
+        <div className="top-container">
+          <Link href="/">
+            <a>
+              <Button className="lighter">
+                <TbArrowBackUp size={24} />
+              </Button>
+            </a>
+          </Link>
+        </div>
+        {isLoading && <Loading bigger />}
+        {!isLoading && error && <DisplayMessage
+          message="Houve um erro na pesquisa"
+          icon={<TbMoodSad size={124} />}
+        />}
+        {!isLoading && videoDetails?.items.length === 0 && <DisplayMessage
+          message="Vídeo não encontrado"
+          icon={<TbMoodSad size={124} />}
+        />}
+        {!isLoading && videoDetails?.items.length > 0 && <div
+          className="details-container fade"
+        >
+          <h1 className="video-title">
+            {videoDetails.items[0].snippet.title}
+          </h1>
+          <div className="iframe-container">
+            <iframe src={`https://www.youtube.com/embed/${videoId}`} />
+          </div>
+          <div className="bottom-container">
+            <div className="main-details">
+              <p className="channel-name">
+                {videoDetails.items[0].snippet.channelTitle}
+              </p>
+              <div className="video-likes">
+                <TbThumbUp size={24} />
+                {videoDetails.items[0].statistics.likeCount}
+              </div>
+            </div>
+            <div className="description">
+              <pre>
+                {videoDetails.items[0].snippet.description}
+              </pre>
+            </div>
+            <div className="video-views">
+              <span>
+                {`${videoDetails.items[0].statistics.viewCount} visualizações`}
+              </span>
+            </div>
+          </div>
+        </div>}
+      </Container>
+    </PageContainer>
+  );
+}
---
+++ src/styles/pages/videoDetails.js
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+
+export default styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+
+  .top-container {
+    position: absolute;
+    top: 132px;
+    left: 12px;
+    display: flex;
+    align-items: center;
+    gap: 24px;
+  }
+
+  .details-container {
+    width: 100%;
+    max-width: 780px;
+    padding: 12px;
+    margin: 0px 12px;
+
+    .video-title {
+      background-color: ${({ theme }) => theme.colors.lighter};
+      font-size: 20px;
+      padding: 12px;
+      border-radius: 8px 8px 0px 0px;
+    }
+
+    .iframe-container {
+      position: relative;
+      padding-bottom: 56.25%;
+
+      iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    .bottom-container {
+      background-color: ${({ theme }) => theme.colors.lighter};
+      padding: 12px;
+      border-radius: 0px 0px 8px 8px;
+      box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px,
+        rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
+
+      .main-details {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 12px 0px;
+        margin-bottom: 24px;
+        border-bottom: 2px solid rgba(0, 0, 0, 0.1);
+
+        .channel-name {
+          font-size: 20px;
+        }
+
+        .video-likes {
+          display: flex;
+          align-items: center;
+          gap: 4px;
+        }
+      }
+
+      .description {
+        padding-bottom: 12px;
+        margin-bottom: 12px;
+        border-bottom: 2px solid rgba(0, 0, 0, 0.1);
+      }
+
+      .video-views {
+        font-weight: 300;
+      }
+    }
+  }
+`;
